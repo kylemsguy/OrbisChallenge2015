98d77abe9eb644495d261f42115e02c381554737
Kyle Zhou <kylezhou2002@hotmail.com>
2015-10-03 14:23:42 -0400

    removed useless stuff

--- botplayer/PlayerAI.java
+++ botplayer/PlayerAI.java
@@ -1,15 +1,27 @@
+import com.orbischallenge.game.enums.*;
+
 import java.util.*;
 import java.awt.Point;
 
 public class PlayerAI extends ClientAI {
+    private Queue<Move> moveQueue = new ArrayDeque<>();
+
 	public PlayerAI() {
 		//Write your initialization here
+        moveQueue.add(Move.FACE_DOWN);
+        moveQueue.add(Move.FORWARD);
+        moveQueue.add(Move.FORWARD);
+        moveQueue.add(Move.FORWARD);
+        moveQueue.add(Move.FORWARD);
+        moveQueue.add(Move.LASER);
+
 	}
 
 	@Override
 	public Move getMove(Gameboard gameboard, Opponent opponent, Player player) throws NoItemException, MapOutOfBoundsException {
-		
+
+        Move nextMove = moveQueue.remove();
 		//Write your AI here
-		return Move.NONE;
+		return nextMove;
 	}
 }
