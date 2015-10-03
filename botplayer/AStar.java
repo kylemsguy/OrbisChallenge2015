a496e7e632f75377ceff3975c5bb30829c277e48
Kyle Zhou <kylezhou2002@hotmail.com>
2015-10-03 14:58:34 -0400

    more work on stuff

--- botplayer/AStar.java
+++ botplayer/AStar.java
@@ -1,3 +1,6 @@
+import com.orbischallenge.engine.gameboard.*;
+
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -5,7 +8,20 @@ public class AStar {
 
 
 	public static class heuristics{
-		public static int manhattenDistance(GameObjects start, GameObjects end, Gameboard gameboard){
+        /**
+         * Returns the shortest manhatten distance from start to end on the game board.
+         *
+         * @param start Start position
+         * @param end Target position
+         * @param board The game board (torus)
+         * @return shortest manhatten distance between points
+         */
+		public static int manhattenDistance(Point start, Point end, Gameboard board){
+            int width = board.getWidth();
+            int height = board.getHeight();
+
+            
+
 			return 0;
 		}
 	}
--- botplayer/PlayerAI.java
+++ botplayer/PlayerAI.java
@@ -4,10 +4,12 @@ import java.util.*;
 import java.awt.Point;
 
 public class PlayerAI extends ClientAI {
+    private AStar pathfinder = new AStar();
     private Queue<Move> moveQueue = new ArrayDeque<>();
 
 	public PlayerAI() {
 		//Write your initialization here
+
         moveQueue.add(Move.FACE_DOWN);
         moveQueue.add(Move.FORWARD);
         moveQueue.add(Move.FORWARD);
@@ -20,7 +22,14 @@ public class PlayerAI extends ClientAI {
 	@Override
 	public Move getMove(Gameboard gameboard, Opponent opponent, Player player) throws NoItemException, MapOutOfBoundsException {
 
-        Move nextMove = moveQueue.remove();
+        // Execute queue
+        Move nextMove;
+        try{
+            nextMove = moveQueue.remove();
+        } catch(NoSuchElementException e){
+            nextMove = Move.NONE;
+            System.err.println("Empty Queue");
+        }
         System.out.println(nextMove);
 		//Write your AI here
 		return nextMove;
